66f109eb42ba34671ab3f0877486d8ea03cb9d0a
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2019-05-17 00:08:14 +0800

    added faq page

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,9 @@ Route::group(['middleware' => ['student']], function()
     Route::match(['get', 'post'],'/student/calendar', 'TransactionFormsController@adminCalendar');
     Route::match(['get', 'post'],'/student/contact', 'PagesController@studentContact');
     Route::post('feedback','FeedbacksController@feedback');
+
+    Route::view('/student/faqs', 'student.faq');
+
     //Route::match(['get', 'post'],'/student/equipment', 'EquipmentsController@showArtEquipment');
     Route::match(['get', 'post'],'/student/cart', 'PagesController@studentCart');
     Route::match(['get', 'post'],'/student/cart2', 'PagesController@studentCart2');
