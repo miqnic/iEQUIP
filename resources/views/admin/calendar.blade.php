a226709552b65271177b1d58876233fdc2ad6d39
Nicole Kaye Bilon <nkrodriguez.bilon@gmail.com>
2019-05-17 16:38:26 +0800

    updated calendar to integrate the rest of the categories

--- resources/views/admin/calendar.blade.php
+++ resources/views/admin/calendar.blade.php
@@ -81,11 +81,11 @@
                   {
                       @if($form->transaction_id == $equipment->transaction_id)
                         @if($form->user_id == $user->user_id)
-                          title : '{{$form->transaction_id}} | {{$user->last_name}}',
+                          title : '{{$form->transaction_id}} | {{$user->first_name}} {{$user->last_name}}',
                           resourceId: '{{$equipment->equipID}}',
                         @endif
                       @endif
-                      start : '{{$form->start_date}}T{{$form->start_time}}',
+                      start: '{{$form->start_date}}T{{$form->start_time}}',
                       end: '{{$form->due_date}}T{{$form->end_time}}'
                   },
                 @endforeach
@@ -200,27 +200,32 @@
         eventTextColor: 'white',
         resourceGroupField: 'itemName',
         resources: [
-            { id: 'a', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0001' },
-            { id: 'b', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0002' },
-            { id: 'c', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0003' },
-            { id: 'd', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0004' },
-            { id: 'e', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0005' },
-            { id: 'f', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0006' },
-            { id: 'g', itemName: 'Sony Playstation 4 Slim Console', title: 'SONPLA0007' },
-            { id: 'h', itemName: 'Xbox One X Console', title: 'XBOONE0001' },
-            { id: 'i', itemName: 'Xbox One X Console', title: 'XBOONE0002' },
-            { id: 'j', itemName: 'Xbox One X Console', title: 'XBOONE0003' },
-            { id: 'k', itemName: 'Xbox One X Console', title: 'XBOONE0004' },
-            { id: 'l', itemName: 'Xbox One X Console', title: 'XBOONE0005' },
-            { id: 'm', itemName: 'Xbox One X Console', title: 'XBOONE0006' },
-            { id: 'n', itemName: 'Xbox One X Console', title: 'XBOONE0007' },
-            { id: 'q', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0001' },
-            { id: 'r', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0002' },
-            { id: 's', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0003' },
-            { id: 't', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0004' },
-            { id: 'u', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0005' },
-            { id: 'v', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0006' },
-            { id: 'w', itemName: 'Razer Kraken Pro Headphones', title: 'RAZKRA0007' },
+          @foreach($equipments as $equipment)
+            @if($equipment->equip_category=="GAMING")
+            { id: '{{$equipment->equipID}}', itemName: '{{$equipment->equip_name}}', title: '{{$equipment->equipID}}' },
+            @endif
+          @endforeach
+        ],
+        eventTextColor: 'white',
+        events : [
+          @foreach($users as $user)
+            @foreach($equipments as $equipment)
+              @if($equipment->equip_category=="GAMING")
+                @foreach($transaction_forms as $form)
+                  {
+                      @if($form->transaction_id == $equipment->transaction_id)
+                        @if($form->user_id == $user->user_id)
+                          title: '{{$form->transaction_id}} | {{$user->first_name}} {{$user->last_name}}',
+                          resourceId: '{{$equipment->equipID}}',
+                        @endif
+                      @endif
+                      start : '{{$form->start_date}}T{{$form->start_time}}',
+                      end: '{{$form->due_date}}T{{$form->end_time}}'
+                  },
+                @endforeach
+              @endif
+            @endforeach
+          @endforeach
         ]
         });
 
@@ -262,27 +267,32 @@
         eventTextColor: 'white',
         resourceGroupField: 'itemName',
         resources: [
-            { id: 'a', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0001' },
-            { id: 'b', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0002' },
-            { id: 'c', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0003' },
-            { id: 'd', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0004' },
-            { id: 'e', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0005' },
-            { id: 'f', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0006' },
-            { id: 'g', itemName: 'Apple Macbook Pro Laptop', title: 'MACPRO0007' },
-            { id: 'h', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0001' },
-            { id: 'i', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0002' },
-            { id: 'j', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0003' },
-            { id: 'k', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0004' },
-            { id: 'l', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0005' },
-            { id: 'm', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0006' },
-            { id: 'n', itemName: 'Microsoft New Surface Pro 2017 Laptop', title: 'MICSUR0007' },
-            { id: 'q', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0001' },
-            { id: 'r', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0002' },
-            { id: 's', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0003' },
-            { id: 't', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0004' },
-            { id: 'u', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0005' },
-            { id: 'v', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0006' },
-            { id: 'w', itemName: 'Logitech M353 Mouse', title: 'LOGMOU0007' },
+          @foreach($equipments as $equipment)
+            @if($equipment->equip_category=="LAPTOP")
+            { id: '{{$equipment->equipID}}', itemName: '{{$equipment->equip_name}}', title: '{{$equipment->equipID}}' },
+            @endif
+          @endforeach
+        ],
+        eventTextColor: 'white',
+        events : [
+          @foreach($users as $user)
+            @foreach($equipments as $equipment)
+              @if($equipment->equip_category=="LAPTOP")
+                @foreach($transaction_forms as $form)
+                  {
+                      @if($form->transaction_id == $equipment->transaction_id)
+                        @if($form->user_id == $user->user_id)
+                          title: '{{$form->transaction_id}} | {{$user->first_name}} {{$user->last_name}}',
+                          resourceId: '{{$equipment->equipID}}',
+                        @endif
+                      @endif
+                      start : '{{$form->start_date}}T{{$form->start_time}}',
+                      end: '{{$form->due_date}}T{{$form->end_time}}'
+                  },
+                @endforeach
+              @endif
+            @endforeach
+          @endforeach
         ]
         });
 
@@ -324,27 +334,32 @@
         eventTextColor: 'white',
         resourceGroupField: 'itemName',
         resources: [
-            { id: 'a', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0001' },
-            { id: 'b', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0002' },
-            { id: 'c', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0003' },
-            { id: 'd', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0004' },
-            { id: 'e', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0005' },
-            { id: 'f', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0006' },
-            { id: 'g', itemName: 'Spalding NBA Official Game Ball Basketball', title: 'SPANBA0007' },
-            { id: 'h', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0001' },
-            { id: 'i', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0002' },
-            { id: 'j', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0003' },
-            { id: 'k', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0004' },
-            { id: 'l', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0005' },
-            { id: 'm', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0006' },
-            { id: 'n', itemName: 'Stiga Pro Carbon Table Tennis Racket', title: 'STIPRO0007' },
-            { id: 'q', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0001' },
-            { id: 'r', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0002' },
-            { id: 's', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0003' },
-            { id: 't', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0004' },
-            { id: 'u', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0005' },
-            { id: 'v', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0006' },
-            { id: 'w', itemName: 'Decathlon FT 950 Indoor Table Tennis Table', title: 'DECIND0007' },
+          @foreach($equipments as $equipment)
+            @if($equipment->equip_category=="SPORTS")
+            { id: '{{$equipment->equipID}}', itemName: '{{$equipment->equip_name}}', title: '{{$equipment->equipID}}' },
+            @endif
+          @endforeach
+        ],
+        eventTextColor: 'white',
+        events : [
+          @foreach($users as $user)
+            @foreach($equipments as $equipment)
+              @if($equipment->equip_category=="SPORTS")
+                @foreach($transaction_forms as $form)
+                  {
+                      @if($form->transaction_id == $equipment->transaction_id)
+                        @if($form->user_id == $user->user_id)
+                          title: '{{$form->transaction_id}} | {{$user->first_name}} {{$user->last_name}}',
+                          resourceId: '{{$equipment->equipID}}',
+                        @endif
+                      @endif
+                      start : '{{$form->start_date}}T{{$form->start_time}}',
+                      end: '{{$form->due_date}}T{{$form->end_time}}'
+                  },
+                @endforeach
+              @endif
+            @endforeach
+          @endforeach
         ]
         });
 
@@ -386,20 +401,32 @@
         eventTextColor: 'white',
         resourceGroupField: 'itemName',
         resources: [
-            { id: 'a', itemName: 'Swivel Chair', title: 'SWICHA0001' },
-            { id: 'b', itemName: 'Swivel Chair', title: 'SWICHA0002' },
-            { id: 'c', itemName: 'Swivel Chair', title: 'SWICHA0003' },
-            { id: 'd', itemName: 'Swivel Chair', title: 'SWICHA0004' },
-            { id: 'e', itemName: 'Swivel Chair', title: 'SWICHA0005' },
-            { id: 'f', itemName: 'Swivel Chair', title: 'SWICHA0006' },
-            { id: 'g', itemName: 'Swivel Chair', title: 'SWICHA0007' },
-            { id: 'h', itemName: 'Whiteboard Marker', title: 'WHIMAR0001' },
-            { id: 'i', itemName: 'Whiteboard Marker', title: 'WHIMAR0002' },
-            { id: 'j', itemName: 'Whiteboard Marker', title: 'WHIMAR0003' },
-            { id: 'k', itemName: 'Whiteboard Marker', title: 'WHIMAR0004' },
-            { id: 'l', itemName: 'Whiteboard Marker', title: 'WHIMAR0005' },
-            { id: 'm', itemName: 'Whiteboard Marker', title: 'WHIMAR0006' },
-            { id: 'n', itemName: 'Whiteboard Marker', title: 'WHIMAR0007' },
+          @foreach($equipments as $equipment)
+            @if($equipment->equip_category=="MISC")
+            { id: '{{$equipment->equipID}}', itemName: '{{$equipment->equip_name}}', title: '{{$equipment->equipID}}' },
+            @endif
+          @endforeach
+        ],
+        eventTextColor: 'white',
+        events : [
+          @foreach($users as $user)
+            @foreach($equipments as $equipment)
+              @if($equipment->equip_category=="MISC")
+                @foreach($transaction_forms as $form)
+                  {
+                      @if($form->transaction_id == $equipment->transaction_id)
+                        @if($form->user_id == $user->user_id)
+                          title: '{{$form->transaction_id}} | {{$user->first_name}} {{$user->last_name}}',
+                          resourceId: '{{$equipment->equipID}}',
+                        @endif
+                      @endif
+                      start : '{{$form->start_date}}T{{$form->start_time}}',
+                      end: '{{$form->due_date}}T{{$form->end_time}}'
+                  },
+                @endforeach
+              @endif
+            @endforeach
+          @endforeach
         ]
         });
 
@@ -409,7 +436,11 @@
 @endsection
 
 @section('navi')
-    @include('inc.naviAdmin')
+    @if(auth()->user()->access_role == 'ADMIN')
+      @include('inc.naviAdmin')
+    @else 
+      @include('inc.naviStudent')
+    @endif
 @endsection
 
 @section('content')
